b517e3cfbab873fbf3294bd978ba7dcf7ea61cb2
BHSon9841 <114989795+BHSon9841@users.noreply.github.com>
2024-11-23 07:56:58 +0700

    Update ProductController.java

--- src/main/java/com/example/btl/controller/ProductController.java
+++ src/main/java/com/example/btl/controller/ProductController.java
@@ -1,4 +1,4 @@
-package com.example.btl.product;
+package com.example.btl.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
